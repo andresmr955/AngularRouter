b73eba028687cc09ad36998afe4030c248421f31
Andres Marquez <andresmr955@gmail.com>
2025-07-08 18:30:10 -0400

    creating routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,14 +15,7 @@ import { TimeAgoPipe } from './pipes/time-ago.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
 import { TimeInterceptor } from './interceptors/time.interceptor';
 import { TokenInterceptor } from './interceptors/token.interceptor';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { CategoryComponent } from './pages/category/category.component';
-import { MyCartComponent } from './pages/my-cart/my-cart.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { RecoveryComponent } from './pages/recovery/recovery.component';
-import { ProfileComponent } from './pages/profile/profile.component';
+
 
 @NgModule({
   declarations: [
@@ -34,14 +27,6 @@ import { ProfileComponent } from './pages/profile/profile.component';
     ReversePipe,
     TimeAgoPipe,
     HighlightDirective,
-    HomeComponent,
-    NotFoundComponent,
-    CategoryComponent,
-    MyCartComponent,
-    LoginComponent,
-    RegisterComponent,
-    RecoveryComponent,
-    ProfileComponent
   ],
   imports: [
     BrowserModule,
