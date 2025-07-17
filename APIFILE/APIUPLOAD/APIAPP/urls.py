c6ee3d7696a11e3431ed4a1e02dca81ea6d79bf0
Andres Marquez <andresmr955@gmail.com>
2025-07-17 13:55:11 -0400

    creating 2 views one for regular pdf ann other in the body of request

--- APIFILE/APIUPLOAD/APIAPP/urls.py
+++ APIFILE/APIUPLOAD/APIAPP/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import upload_file, upload_success, FileUploadView
+from .views import upload_file, upload_success, FileUploadView, UploadRawPdfView
 
 urlpatterns = [
     path('upload/', upload_file, name='upload'),
     path('success/', upload_success, name='success'),
-    path('uploadAPI/', FileUploadView.as_view(), name="file-upload")
+    path('uploadAPI/', FileUploadView.as_view(), name="file-upload"),
+    path('uploadAPIRAW/', UploadRawPdfView.as_view(), name="file-upload")
 ]
--- APIFILE/APIUPLOAD/APIAPP/views.py
+++ APIFILE/APIUPLOAD/APIAPP/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from .forms import UploadFileForm
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.response import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import status
 from .serializers import UploadedFileSerializer
-
+from django.views import View
+from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 def upload_file(request):
     if request.method == 'POST':
@@ -30,3 +33,15 @@ class FileUploadView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@method_decorator(csrf_exempt, name='dispatch')
+class UploadRawPdfView(View):
+    def post(self, request):
+        content_type = request.META.get('CONTENT_TYPE', '')
+        if content_type != 'application/pdf':
+            return HttpResponse('Invalid content type', status=415)
+        
+        pdf_data = request.body  # Aquí tienes el archivo en bruto
+        # Guarda pdf_data en archivo o base de datos
+        
+        return HttpResponse('PDF received', status=201)
